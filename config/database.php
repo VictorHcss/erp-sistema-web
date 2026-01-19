e06f617d5a428c77e72293ccfd526eacd9baf5aa
VictorHnrq <henvictor2@gmail.com>
2026-01-18 21:57:30 -0300

    Modify database.php to dump environment variables Test
    Replaced database connection code with var_dump for environment variables and added exit.

--- config/database.php
+++ config/database.php
@@ -1,21 +1,12 @@
 <?php
 
-$host = getenv('MYSQLHOST');
-$db   = getenv('MYSQLDATABASE');
-$user = getenv('MYSQLUSER');
-$pass = getenv('MYSQLPASSWORD');
-$port = getenv('MYSQLPORT');
+var_dump(
+    getenv('MYSQLHOST'),
+    getenv('MYSQLDATABASE'),
+    getenv('MYSQLUSER'),
+    getenv('MYSQLPASSWORD'),
+    getenv('MYSQLPORT')
+);
 
-try {
-    $pdo = new PDO(
-        "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4",
-        $user,
-        $pass,
-        [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-        ]
-    );
-} catch (PDOException $e) {
-    die("Erro ao conectar ao banco de dados.");
-}
+exit;
+?>
